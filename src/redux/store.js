249f31ee77979581cc16d8ff88b42f800bb0c90d
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-02-28 23:25:57 +0700

    Add thunk pakage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/booksSlice';
 import categoriesReducer from './categories/categoriesSlice';
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './appReducers';
 
 const store = configureStore({
   reducer: {
