fcdc02e9bc7a11bc03609f7489cb47e1f03cd4f9
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-03-02 22:36:02 +0700

    Add styling for project

--- src/components/Book.component.jsx
+++ src/components/Book.component.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { deleteBook } from 'redux/books/booksSlice';
+import { CircularProgress } from '@mui/material';
 import './Book.styles.scss';
 
 function Book({ book, id }) {
@@ -13,12 +14,30 @@ function Book({ book, id }) {
 
   return (
     <div className="book-container">
-      <div className="info">
-        <span>{category}</span>
-        <h3>{title}</h3>
-        <span>{author}</span>
+      <div className="sec-1">
+        <div className="info">
+          <span className="category">{category}</span>
+          <h3>{title}</h3>
+          <span className="author">{author}</span>
+        </div>
+        <div className="btn-book">
+          <button type="button">Comment</button>
+          <button onClick={removeHandler} type="button">Remove</button>
+          <button type="button">Edit</button>
+        </div>
       </div>
-      <button onClick={removeHandler} type="button">Remove</button>
+      <div className="sec-2">
+        <CircularProgress className="progressbar" size="80px" variant="determinate" value={75} />
+        <div>
+          <h4>64%</h4>
+          <span>Completed</span>
+        </div>
+      </div>
+      <div className="sec-3">
+        <span>Current chapter</span>
+        <h4>Chapter 17</h4>
+        <button type="button">update progress</button>
+      </div>
     </div>
   );
 }
